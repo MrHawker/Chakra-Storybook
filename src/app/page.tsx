09aa289f35c9d3e6eb67ccee9669b9823d7c1c46
MrHawker <bonbon.muoiot@gmail.com>
2024-12-17 00:39:26 +0700

    Added storybook

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 
 
 import { HydrateClient } from "~/trpc/server";
+import { Button } from "./_components/ui/button";
 
 export default async function Home() {
 
   return (
     <HydrateClient>
       <div>HELLO WORLD</div>
+      <Button>BRUH</Button>
     </HydrateClient>
   );
 }
